ed05f1838b80733fd6562e062d7a7521ed923718
pntbr <stephane.langlois@scopyleft.fr>
2019-03-08 17:14:22 +0100

    dom loaded

---
+++ js-premier-pas/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', event => {
+    // ici le DOM est chargé, on peut utiliser document.querySelector()
+})
+
+// ici le DOM n'est pas encore chargé, on ne peut pas utiliser document.querySelector()
