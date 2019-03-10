1ddb423cbb791e185624e26983c6fc6a01226d91
Pierre de MAULMONT <pierre.de-maulmont@ac-paris.fr>
2019-03-10 19:21:29 +0100

    exercice pour écrire séparément js et html

---
+++ js-premier-pas/je_separe_mon_code.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', event => 
+   {
+      // ici le DOM est chargé :
+   
+      EPLE = [  
+       {'nom': 'ARAGO', 'UAI': '0750680G', 'latitude': '48.847936', 'longitude' : '2.394438'},    
+       {'nom': 'La GARRIGUETTE', 'UAI': '0300050S', 'latitude': '43.7477966', 'longitude' : '4.2245153'},
+       {'nom': 'Boris VIAN', 'UAI': '0140070A', 'latitude': '49.0759528', 'longitude' : '-0.0794074'}
+      ]
+     
+      write = (msg) => document.write(`<ul><li><strong><em>${msg}</em></strong> </li></ul><br>`)
+     
+      displayEPLEwithUAI = (etablissements) => etablissements.map(etablissement => 
+      write(`EPLE : ${etablissement.nom} - UAI : ${etablissement.UAI} - UAI : ${etablissement.latitude} - UAI : ${etablissement.longitude}`))
+         
+      displayEPLEwithUAI(EPLE)   
+   })
+
+   document.addEventListener('DOMContentLoaded', event => 
+   {
+      // ici le DOM est chargé :
+   
+      document.querySelector('header').innerText
+   })
